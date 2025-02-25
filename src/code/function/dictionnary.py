26b2928d66cccbf0db7addecfcacb75b59c8a03c
Tiiiagooo <andre.decarvalhobene@gmail.com>
2025-02-25 22:30:56 +0100

    Ajout nouveaux code et nouvelles fonction pour la récupération des artistes

--- src/code/function/dictionnary.py
+++ src/code/function/dictionnary.py
@@ -1,11 +1,11 @@
 from function.utils import os, json
 
-def get_dictionnary(PATH_JSON):
-    if os.path.exists(PATH_JSON+"dictionnaire_song.json"):
-        with open(PATH_JSON+"dictionnaire_song.json", "r") as json_file:
-            dictionnaire_song = json.load(json_file)
+def get_dictionnary(PATH_JSON, dictionnary_name):
+    if os.path.exists(PATH_JSON+dictionnary_name):
+        with open(PATH_JSON+dictionnary_name, "r") as json_file:
+            dictionnaire = json.load(json_file)
     else:
-        dictionnaire_song = dict()
-        with open(PATH_JSON+"dictionnaire_song.json", "w") as json_file:
-            json.dump(dictionnaire_song, json_file, indent=4)
-    return dictionnaire_song
+        dictionnaire = dict()
+        with open(PATH_JSON+dictionnary_name, "w") as json_file:
+            json.dump(dictionnaire, json_file, indent=4)
+    return dictionnaire
--- src/code/function/scrapping.py
+++ src/code/function/scrapping.py
@@ -6,3 +6,20 @@ def scrapping_find_lyrics(lyrics_url, headers):
     lyrics_div = soup.find_all("div", {"class": "Lyrics-sc-37019ee2-1 jRTEBZ"})
     liste_parole = [div.get_text(separator="\n").strip() for div in lyrics_div]
     return liste_parole
+
+def get_url(URL_BASE, years, headers):
+    response = requests.get(URL_BASE+"annee/"+years, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    pagination_links = soup.find_all("a", class_="pagination")
+    next_pages = [URL_BASE+link.get("href") for link in pagination_links if link.get("href")]
+    if not next_pages:
+        return [URL_BASE + "annee/" + str(years)]
+
+    return next_pages
+
+def scrapping_artiste(URL, headers):
+    response = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    artiste_div = soup.find_all("span", class_="single-artiste")
+    liste_artiste = [artiste.get_text().strip() for artiste in artiste_div]
+    return liste_artiste
